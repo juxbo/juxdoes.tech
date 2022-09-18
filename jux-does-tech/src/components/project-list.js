1d4ae055cc4c99012d2c093bc62f32d6a8ce3707
Justin Boehm <justin.boehm@hotmail.de>
2022-09-18 14:30:01 +0200

    Fix white bg

--- jux-does-tech/src/components/project-list.js
+++ jux-does-tech/src/components/project-list.js
@@ -27,44 +27,47 @@ const ProjectList = ({ posts, startOffset }) => {
         <AnimatedHeader text="Projects." />
       </ParallaxLayer>
       <ParallaxLayer offset={startOffset} speed={0.4}>
-        <div className="section bg-white">
-          <h2>My latest projects</h2>
-          <ol style={{ listStyle: `none` }}>
-            {/* TODO: Paginate (pagesize 4) */}
-            {posts.slice(0, 4).map(post => {
-              const title = post.frontmatter.title || post.fields.slug
+        <div className="section">
+          <div className="bg-white">
+            <h2>My latest projects</h2>
+            <ol style={{ listStyle: `none` }}>
+              {/* TODO: Paginate (pagesize 4) */}
+              {posts.slice(0, 4).map(post => {
+                const title = post.frontmatter.title || post.fields.slug
 
-              return (
-                <li key={post.fields.slug}>
-                  <article
-                    className="post-list-item"
-                    itemScope
-                    itemType="http://schema.org/Article"
-                  >
-                    <header>
-                      <h2>
-                        <Link to={post.fields.slug} itemProp="url">
-                          <span itemProp="headline">{title}</span>
-                        </Link>
-                      </h2>
-                      <small>{post.frontmatter.date}</small>
-                    </header>
-                    <section>
-                      <p
-                        dangerouslySetInnerHTML={{
-                          __html: post.frontmatter.description || post.excerpt,
-                        }}
-                        itemProp="description"
-                      />
-                    </section>
-                  </article>
-                </li>
-              )
-            })}
-            {/* <li>
+                return (
+                  <li key={post.fields.slug}>
+                    <article
+                      className="post-list-item"
+                      itemScope
+                      itemType="http://schema.org/Article"
+                    >
+                      <header>
+                        <h2>
+                          <Link to={post.fields.slug} itemProp="url">
+                            <span itemProp="headline">{title}</span>
+                          </Link>
+                        </h2>
+                        <small>{post.frontmatter.date}</small>
+                      </header>
+                      <section>
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html:
+                              post.frontmatter.description || post.excerpt,
+                          }}
+                          itemProp="description"
+                        />
+                      </section>
+                    </article>
+                  </li>
+                )
+              })}
+              {/* <li>
               <button>Read all posts</button>
             </li> */}
-          </ol>
+            </ol>
+          </div>
         </div>
       </ParallaxLayer>
     </>
